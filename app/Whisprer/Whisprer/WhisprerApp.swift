61346c589e8961e710fe3fae088e3f8e5c53e76a
Alex <your-personal@email.com>
2026-03-22 18:23:14 +0100

    Initial setup

---
+++ app/Whisprer/Whisprer/WhisprerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisprerApp.swift
+//  Whisprer
+//
+//  Created by Alex Dritsas on 22/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WhisprerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
